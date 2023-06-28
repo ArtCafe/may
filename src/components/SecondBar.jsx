ee1c42ce86724fe6fc84b963e9049f1bb64f9ab2
Art <121219238+ArtCafe@users.noreply.github.com>
2023-06-28 15:33:09 +0300

    Update SecondBar.jsx

--- src/components/SecondBar.jsx
+++ src/components/SecondBar.jsx
@@ -13,24 +13,24 @@ export default function SecondBar() {
 <ul className="w3-ul w3-hoverable w3-white">
 
   <li className="w3-padding-16">
-    <img src="https://www.w3schools.com/w3images/workshop.jpg" alt="Image" className="w3-left w3-margin-right" style={{width:"50px"}}/>
+    <img src="https://www.w3schools.com/w3images/workshop.jpg" alt="age" className="w3-left w3-margin-right" style={{width:"50px"}}/>
     <Link to = "/users" className="w3-large">Users</Link><br/>
     <span>Informatie utilizatori</span>
   </li>
 
   <li className="w3-padding-16">
-    <img src="https://www.w3schools.com/w3images/gondol.jpg" alt="Image" className="w3-left w3-margin-right" style={{width:"50px"}}/>
+    <img src="https://www.w3schools.com/w3images/gondol.jpg" alt="age" className="w3-left w3-margin-right" style={{width:"50px"}}/>
     <Link to="/redactarepost" className="w3-large">Post Redactare </Link><br/>
     <span>Praes tinci sed</span>
   </li> 
 
   <li className="w3-padding-16">
-    <img src="https://www.w3schools.com/w3images/skies.jpg" alt="Image" className="w3-left w3-margin-right" style={{width:"50px"}}/>
+    <img src="https://www.w3schools.com/w3images/skies.jpg" alt="age" className="w3-left w3-margin-right" style={{width:"50px"}}/>
     <Link to = "/profile" className="w3-large">Profil Redactare</Link><br/>
     <span>Ultricies congue</span>
   </li>   
   <li className="w3-padding-16 w3-hide-medium w3-hide-small">
-    <img src="https://www.w3schools.com/w3images/woods.jpg" alt="Image" className="w3-left w3-margin-right" style={{width:"50px"}}/>
+    <img src="https://www.w3schools.com/w3images/woods.jpg" alt="age" className="w3-left w3-margin-right" style={{width:"50px"}}/>
     <span className="w3-large">Mingsum</span><br/>
     <span>Lorem ipsum dipsum</span>
   </li>  
