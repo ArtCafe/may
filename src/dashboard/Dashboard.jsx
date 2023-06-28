f84ff698e51014d3b5f8e2794a5776e217984516
Art <121219238+ArtCafe@users.noreply.github.com>
2023-06-28 15:34:18 +0300

    Update Dashboard.jsx

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import { useLocation,  } from "react-router-dom";
-import Modal from '../moreUseComponet/Modal/Modalhoc'
+
 import PostForm from '../components/PostForm/PostForm';
 
 import SecondBar from '../components/SecondBar';
