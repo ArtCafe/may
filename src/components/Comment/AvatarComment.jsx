547f8620ae7acf100299966ebec41d33bb618aee
Art <121219238+ArtCafe@users.noreply.github.com>
2023-06-28 15:20:06 +0300

    Update AvatarComment.jsx

--- src/components/Comment/AvatarComment.jsx
+++ src/components/Comment/AvatarComment.jsx
@@ -6,15 +6,15 @@ import React from 'react'
 const AvatarComment = ({ user }) => {
   return (
     <div className="avatar_comment">
-    {/* <img src={user.avatar} alt="avatar" />*/}  
+    
     <img 
-    //src="https://www.w3schools.com/w3images/workshop.jpg"
+ 
     src={user?.avatar}
-     alt="Image" className="w3-left w3-margin-right" style={{width:"50px"}}/>
+     alt="age" className="w3-left w3-margin-right" style={{width:"50px"}}/>
       <small className="d-block text-break">
-       {/*  <Link to={`/profile/${user._id}`}>
+      
           {user.name}
-        </Link>*/}{user?.name}
+        {user?.name}
       </small>
     </div>
   )
