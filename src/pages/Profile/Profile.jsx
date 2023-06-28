8cb207eba7d4fe4bbfc9b5dbec157e0ccf6541ca
Art <121219238+ArtCafe@users.noreply.github.com>
2023-06-28 15:45:47 +0300

    Update Profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -8,44 +8,7 @@ export default function Profile() {
     <> 
     <div>
 
-  <div className="w3-row">
-<div className="w3-col l8 s12"  >
- 
-
-
-</div>
-</div>
-<div className="w3-col l4">
-
-  <div className="w3-card-4 w3-margin w3-white">
- 
-   
-    </div>
-  </div>
- </div>
-
-</div>
-
-
-<div className="w3-col l4">
-
-  <div className="w3-card w3-margin w3-margin-top">
- 
-  </div>
-  
-
-  <div className="w3-card w3-margin">
-    
-  </div>
-
-  <div className="w3-card w3-margin">
-   
-    <div className="w3-container w3-white">
-   
-    </div>
-  </div>
-  
-
+<h2>prrofile</h2>
 </div>
 
   </>
