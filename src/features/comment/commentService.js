dbe9a1e8d012748eabe164a1c7b0800406e3c6c7
Art <121219238+ArtCafe@users.noreply.github.com>
2023-06-28 15:58:37 +0300

    Update commentService.js

--- src/features/comment/commentService.js
+++ src/features/comment/commentService.js
@@ -6,11 +6,6 @@ const API_URL = '/api/comments/'
 const createComment = async (commentData, token) => {
 
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }
 
   const response = await axios.post(API_URL, commentData)
 
